d0a6fd3a1b4ccfbb6ffc83441fc1fea49ee48d50
shineking9099 <shine301pk@gmail.com>
2025-05-22 13:56:48 +0500

    Update backend code

--- app.js
+++ app.js
@@ -6,8 +6,11 @@ require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST"],
+  credentials: true
+}));
 app.use(bodyParser.json());
 
 // MongoDB Connection
